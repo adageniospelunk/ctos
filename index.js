dbe4e7a31df0687350572e59dad86489a5329246
ctos <cots@ctos.com>
2025-10-27 21:39:37 +0300

    Add --version and -v flag support

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log('Usage: ctos echo <something>');
+  console.log('Usage: ctosooa echo <something>');
   process.exit(1);
 }
 
 const command = args[0];
 
-if (command === 'echo') {
+if (command === '--version' || command === '-v') {
+  const pkg = require('./package.json');
+  console.log(pkg.version);
+} else if (command === 'echo') {
   const rest = args.slice(1);
   console.log(rest.join(' '));
 } else {
