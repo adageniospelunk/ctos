80026a9f4af49326886b45d7d179184533e061b6
ctos <cots@ctos.com>
2025-10-27 21:33:51 +0300

    Make publish script auto-bump patch version

--- publish.sh
+++ publish.sh
@@ -16,33 +16,9 @@ fi
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 echo "Current version: $CURRENT_VERSION"
 
-# Ask for version bump type
-echo ""
-echo "Select version bump type:"
-echo "1) patch (x.x.X) - Bug fixes"
-echo "2) minor (x.X.0) - New features"
-echo "3) major (X.0.0) - Breaking changes"
-echo "4) skip version bump"
-read -p "Enter choice [1-4]: " choice
-
-case $choice in
-  1)
-    npm version patch
-    ;;
-  2)
-    npm version minor
-    ;;
-  3)
-    npm version major
-    ;;
-  4)
-    echo "Skipping version bump..."
-    ;;
-  *)
-    echo "Invalid choice. Exiting."
-    exit 1
-    ;;
-esac
+# Automatically bump patch version
+echo "Bumping patch version..."
+npm version patch
 
 # Get new version
 NEW_VERSION=$(node -p "require('./package.json').version")
