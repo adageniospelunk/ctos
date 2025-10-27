4eeca25b4ef4c53a881e3adbe105f00430d9dce2
ctos <cots@ctos.com>
2025-10-27 21:25:32 +0300

    Initial commit: Create ctos CLI package

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// parse arguments
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('Usage: ctos echo <something>');
+  process.exit(1);
+}
+
+const command = args[0];
+
+if (command === 'echo') {
+  const rest = args.slice(1);
+  console.log(rest.join(' '));
+} else {
+  console.log(`Unknown command: ${command}`);
+  process.exit(1);
+}
